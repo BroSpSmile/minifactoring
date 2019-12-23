4d60d7a91fba7787893a8d3748d15cbf53905ccf
smile.jing <jingyongxiang@gmail.com>
2019-12-23 23:27:50 +0800

    commit

--- miniprogram/pages/fund/fund.js
+++ miniprogram/pages/fund/fund.js
@@ -1,25 +1,58 @@
 // miniprogram/pages/fund/fund.js
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    service: app.globalData.service,
+    array: [{ value: '', text: '全部' }],
+    index: 0,
+    queryParam: {
+      condition: {detail:{}},
+      pageNum: 1,
+      pageSize: 10
+    },
+    pageInfo: {}
+  },
 
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value,
+      'queryParam.condition.detail.projectStep': this.data.meetingstatus[parseInt(e.detail.value)].value
+    });
+    this.loadProject();
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.cloud.callFunction({
+      name: 'http',
+      data: {
+        endpoint: this.data.service + '/combo/fundStatus',
+        method: 'GET'
+      },
+      complete: res => {
+        var kinds = this.data.array;
+        for (var index in res.result) {
+          kinds.push(res.result[index]);
+        }
+        this.setData({
+          array: kinds
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.loadProject();
   },
 
   /**
@@ -62,5 +95,31 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  loadProject: function () {
+    var request = JSON.parse(JSON.stringify(this.data.queryParam));
+    if (this.data.queryParam.condition.step == -1) {
+      request.condition = {};
+    }
+    wx.cloud.callFunction({
+      name: 'http',
+      data: {
+        endpoint: this.data.service + '/fund/query',
+        method: 'POST',
+        body: JSON.stringify(request)
+      },
+      complete: res => {
+        this.setData({
+          pageInfo: res.result
+        })
+      }
+    })
+  },
+
+  showDetail: function (e) {
+    wx.navigateTo({
+      url: "../funddetail/index?id=" + e.currentTarget.dataset.projectid
+    })
   }
 })
